e9266ad332d84c19dadb89403cfdea045b5369e7
prathamsheth773 <prathamsheth077@gmail.com>
2023-09-01 12:42:25 +0530

    admin_authentication

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-md">Login with Google</button>
+  const { data: session } = useSession()
+  if(!session)
+  {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')}className="bg-white p-2 px-4 rounded-md">Login with Google</button>
+        </div>
       </div>
-    </div>
+  
+    )
+  }
 
+  return(
+    <div>Logged in {session.user.name}</div>
   )
 }
